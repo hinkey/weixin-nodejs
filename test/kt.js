eb9ac9798efde77f83a794c25b1fab7c7126a7b8
hinkey <ad8jk@163.com>
2015-03-25 01:12:35 +0800

    2015/03/24

---
+++ test/kt.js
@@ -0,0 +1,27 @@
+//{
+//    "button":[
+//    {
+//        "type":"click",
+//        "name":"我是谁",
+//        "key":"whoami"
+//    },
+//    {
+//        "name":"菜单",
+//        "sub_button":[
+//            {
+//                "type":"view",
+//                "name":"联系人",
+//                "url":"http://i.oaapi.com/#/search"
+//            },
+//            {
+//                "type":"view",
+//                "name":"我的微博",
+//                "url":"http://weibo.com/4337365"
+//            },
+//            {
+//                "type":"click",
+//                "name":"联系我们",
+//                "key":"whoareyou"
+//            }]
+//    }]
+//}
